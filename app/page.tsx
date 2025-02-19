b58696f8f3abc70b86193787b4c9b418c399e378
Gustavo <gustavo.sperandio25@gmail.com>
2025-02-19 16:24:01 -0300

    Login

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,73 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+"use client";
+import React from "react";
+import { Button, Input, Checkbox, Link, Form } from "@heroui/react";
+import { Eye, EyeClosed } from "lucide-react";
 
 export default function Home() {
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("handleSubmit");
+  };
+
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-        <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
-        <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </div>
-      </div>
-
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="flex w-full max-w-sm flex-col gap-4 rounded-large px-8 pb-10 pt-6">
+        <p className="pb-4 text-left text-3xl font-semibold">
+          Log In
+          <span aria-label="emoji" className="ml-2" role="img">
+            👋
           </span>
-        </Snippet>
+        </p>
+        <Form
+          className="flex flex-col gap-4"
+          validationBehavior="native"
+          onSubmit={handleSubmit}
+        >
+          <Input
+            isRequired
+            label="Email"
+            labelPlacement="outside"
+            name="email"
+            placeholder="Enter your email"
+            type="email"
+            variant="bordered"
+          />
+          <Input
+            isRequired
+            endContent={
+              <button type="button" onClick={toggleVisibility}>
+                {isVisible ? <Eye /> : <EyeClosed />}
+              </button>
+            }
+            label="Password"
+            labelPlacement="outside"
+            name="password"
+            placeholder="Enter your password"
+            type={isVisible ? "text" : "password"}
+            variant="bordered"
+          />
+          <div className="flex w-full items-center justify-between px-1 py-2">
+            <Checkbox defaultSelected name="remember" size="sm">
+              Remember me
+            </Checkbox>
+            <Link className="text-default-500" href="#" size="sm">
+              Forgot password?
+            </Link>
+          </div>
+          <Button className="w-full" color="primary" type="submit">
+            Log In
+          </Button>
+        </Form>
+        <p className="text-center text-small">
+          <Link href="#" size="sm">
+            Create an account
+          </Link>
+        </p>
       </div>
-    </section>
+    </div>
   );
 }
